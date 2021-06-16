13b292a34590aa392f9aec93a3f1f582a1f65b83
Patrick-Croquet <58164643+Patrick-Croquet@users.noreply.github.com>
2021-06-16 14:11:49 +0200

    Initial commit

---
+++ monapp-vue-bootstrap/src/views/Contact.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+      <div class="image">
+      </div>
+      <p></p> 
+      <div class="row ml-4 mr-4">
+        <div class="col-md-6">
+            <base-input placeholder="name@example.com"></base-input>
+        </div>
+        <div class="col-md-6">
+          <base-input placeholder="Disabled" disabled></base-input>
+        </div>
+      </div>
+      <div class="row ml-4 mr-4">
+          <div class="col-md-6">
+              <base-input placeholder="Birthday" prepend-icon="ni ni-zoom-split-in"></base-input>
+          </div>
+          <div class="col-md-6">
+            <base-input placeholder="Birthday" append-icon="ni ni-zoom-split-in"></base-input>
+          </div>
+        </div>
+        <div class="row ml-4 mr-4">
+          <div class="col-md-6">
+              <base-input placeholder="Success" :valid="true"></base-input>
+          </div>
+          <div class="col-md-6">
+            <base-input placeholder="Error input" error="Some error"></base-input>
+          </div>
+        </div>
+    </div> 
+</template>
+<script>
+export default {
+  name: "contact",
+}
+</script>
+<style>
+.image {
+  width: 100%;
+  height: 200px;
+  background-color:aqua;
+  background-image: url('/img/mountains/montagne1.jpg');
+  background-size: cover;
+}
+</style>
---
+++ monapp-vue-bootstrap/src/views/Essai.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <carousel1></carousel1>
+        <carousel2></carousel2>
+    </div>
+</template>
+<script>
+import Carousel1 from "./components/Carousel";
+import Carousel2 from "./components/Carousel";
+
+export default {
+  name: "essai",
+  components: {
+    Carousel1,
+    Carousel2
+  }
+}
+</script>
